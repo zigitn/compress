63bdc414e7c185fef3b474ee24c63a8e79a8e1c8
zigitn <me@5a6c.me>
2022-02-13 21:48:28 +0800

    add option to config compress method

--- compress.go
+++ compress.go
@@ -3,6 +3,7 @@ package compress
 import (
 	"compress/flate"
 	"compress/gzip"
+	"fmt"
 	"strconv"
 	"strings"
 
@@ -10,28 +11,61 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func New() gin.HandlerFunc {
+const (
+	UseBrotli  = "br"
+	UseGzip    = "gzip"
+	UseDeflate = "deflate"
+)
+
+// Option can enable and config each compress method
+// for simple needs please use UseAllBestSpeed or UseAllBestBestCompression
+type Option struct {
+	EnableMethods []string // enabled compress methods, will be matched in order
+	GzipLevel     int
+	BrotliOption  brotli.WriterOptions
+	DeflateOption DeflateOption
+}
+
+type DeflateOption struct {
+	Level int
+	Dict  []byte // leave empty for default
+}
+
+func UseAllBestSpeed() Option {
+	return Option{
+		EnableMethods: []string{UseBrotli, UseGzip, UseDeflate},
+		GzipLevel:     gzip.BestSpeed,
+		BrotliOption:  brotli.WriterOptions{Quality: brotli.BestSpeed},
+		DeflateOption: DeflateOption{Level: flate.BestSpeed},
+	}
+}
+
+func UseAllBestBestCompression() Option {
+	return Option{
+		EnableMethods: []string{UseBrotli, UseGzip, UseDeflate},
+		GzipLevel:     gzip.BestCompression,
+		BrotliOption:  brotli.WriterOptions{Quality: brotli.BestCompression},
+		DeflateOption: DeflateOption{Level: flate.BestCompression},
+	}
+}
+
+// New a compress middleware, will use Option to config
+func New(option Option) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		acceptEncodingsStr := c.GetHeader("accept-encoding")
 		if len(acceptEncodingsStr) == 0 {
 			return
 		}
-		acceptEncodings := strings.Split(acceptEncodingsStr, ",")
-		if len(acceptEncodings) == 0 {
-			return
-		}
 		var responseWriter *ResponseWriter
-		switch strings.TrimSpace(acceptEncodings[0]) {
-		case "gzip":
-			c.Header("Content-Encoding", "gzip")
-			responseWriter = NewGzip(c.Writer, gzip.BestSpeed)
-		case "deflate":
-			c.Header("Content-Encoding", "deflate")
-			responseWriter = NewDeflate(c.Writer, flate.BestSpeed)
-		case "br":
-			c.Header("Content-Encoding", "br")
-			responseWriter = NewBrotli(c.Writer, brotli.BestSpeed)
-		default:
+		for i := range option.EnableMethods {
+			if strings.Contains(acceptEncodingsStr, option.EnableMethods[i]) {
+				fmt.Println(option.EnableMethods[i])
+				c.Header("Content-Encoding", option.EnableMethods[i])
+				methodsMap[option.EnableMethods[i]](c.Writer, option)
+				break
+			}
+		}
+		if responseWriter == nil {
 			return
 		}
 		c.Writer = responseWriter
--- methods.go
+++ methods.go
@@ -8,8 +8,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func NewGzip(writer gin.ResponseWriter, level int) *ResponseWriter {
-	gzipWriter, err := gzip.NewWriterLevel(writer, level)
+var (
+	methodsMap = map[string]func(writer gin.ResponseWriter, option Option) *ResponseWriter{
+		"gzip":    NewGzip,
+		"deflate": NewDeflate,
+		"br":      NewBrotli,
+	}
+)
+
+func NewGzip(writer gin.ResponseWriter, option Option) *ResponseWriter {
+	gzipWriter, err := gzip.NewWriterLevel(writer, option.GzipLevel)
 	if err != nil {
 		panic(err)
 	}
@@ -19,10 +27,19 @@ func NewGzip(writer gin.ResponseWriter, level int) *ResponseWriter {
 	}
 }
 
-func NewDeflate(writer gin.ResponseWriter, level int) *ResponseWriter {
-	deflateWriter, err := flate.NewWriter(writer, level)
-	if err != nil {
-		panic(err)
+func NewDeflate(writer gin.ResponseWriter, option Option) *ResponseWriter {
+	var deflateWriter *flate.Writer
+	var err error
+	if len(option.DeflateOption.Dict) == 0 {
+		deflateWriter, err = flate.NewWriter(writer, option.DeflateOption.Level)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		deflateWriter, err = flate.NewWriterDict(writer, option.DeflateOption.Level, option.DeflateOption.Dict)
+		if err != nil {
+			panic(err)
+		}
 	}
 	return &ResponseWriter{
 		ResponseWriter: writer,
@@ -30,8 +47,8 @@ func NewDeflate(writer gin.ResponseWriter, level int) *ResponseWriter {
 	}
 }
 
-func NewBrotli(writer gin.ResponseWriter, level int) *ResponseWriter {
-	brWriter := brotli.NewWriterLevel(writer, level)
+func NewBrotli(writer gin.ResponseWriter, option Option) *ResponseWriter {
+	brWriter := brotli.NewWriterOptions(writer, option.BrotliOption)
 	return &ResponseWriter{
 		ResponseWriter: writer,
 		ComPressWriter: brWriter,

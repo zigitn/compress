4558d311512bf2aaf44d77e1772331f26ad5b56b
zigitn <me@5a6c.me>
2022-02-15 12:38:23 +0800

    add more exclude option

--- compress.go
+++ compress.go
@@ -4,6 +4,7 @@ import (
 	"compress/flate"
 	"compress/gzip"
 	"fmt"
+	"regexp"
 	"strconv"
 	"strings"
 
@@ -20,11 +21,15 @@ const (
 // Option can enable and config each compress method
 // for simple needs please use UseAllBestSpeed or UseAllBestBestCompression
 type Option struct {
-	EnableMethods []string // enabled compress methods, will be matched in order
-	GzipLevel     int
-	BrotliOption  brotli.WriterOptions
-	DeflateOption DeflateOption
-	HeadFilter    map[string]string
+	EnableMethods    []string // enabled compress methods, will be matched in order
+	GzipLevel        int
+	BrotliOption     brotli.WriterOptions
+	DeflateOption    DeflateOption
+	HeadFilter       map[string]string
+	ExtensionsFilter []string
+	PathFilter       []string
+	PathRegexFilter  []string
+	CustomFilter     []func(c *gin.Context) bool // return true to exclude anything you want
 }
 
 // DeflateOption is used to config deflate
@@ -44,6 +49,7 @@ func UseAllBestSpeed() Option {
 			"Connection": "Upgrade",
 			"Accept":     "text/event-stream",
 		},
+		ExtensionsFilter: []string{".png", ".gif", ".jpeg", ".jpg", ".svg", ".woff", ".woff2", ".ttf", ".eot"},
 	}
 }
 
@@ -58,12 +64,38 @@ func UseAllBestBestCompression() Option {
 			"Connection": "Upgrade",
 			"Accept":     "text/event-stream",
 		},
+		ExtensionsFilter: []string{".png", ".gif", ".jpeg", ".jpg", ".svg", ".woff", ".woff2", ".ttf", ".eot"},
 	}
 }
 
 // New a compress middleware, will use Option to config
 func New(option Option) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		// check extension
+		for _, extension := range option.ExtensionsFilter {
+			if strings.HasSuffix(c.Request.URL.Path, extension) {
+				return
+			}
+		}
+		// check path
+		for _, path := range option.PathFilter {
+			if c.Request.URL.Path == path {
+				return
+			}
+		}
+		// check path regex
+		for _, path := range option.PathRegexFilter {
+			if match, _ := regexp.MatchString(path, c.Request.URL.Path); match {
+				return
+			}
+		}
+		// custom check func
+		for _, f := range option.CustomFilter {
+			if f(c) {
+				return
+			}
+		}
+
 		for k, v := range option.HeadFilter {
 			if strings.Contains(c.GetHeader(k), v) {
 				return
